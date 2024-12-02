2934fbc2262f0bbd9b57324f97b726c05feeb23f
Sigurd Drivstuen <sigdriv06@gmail.com>
2024-12-02 14:55:19 +0100

    Mandag 02.12.24

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,14 @@
+import { AppBar, Stack } from "@mui/material";
+
+export function NavBar() {
+  return (
+    <AppBar position="static">
+      <Stack flex=''>
+
+      <div>NavBar</div>
+
+      <div>NavBar</div>
+      </Stack>
+    </AppBar>
+  );
+}
